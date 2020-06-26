12cbd16009122e5e691b2996000f1a0370b64849
DanielAristy <danielariaty@gmail.com>
2020-06-26 12:29:47 -0500

    Creando componente visuales de la aplicacion para los test

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+/**https://www.youtube.com/watch?v=i9dTeJlIP60
+ * Video de Pruebas unitarias*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void suma(View view) {
+    }
+
+    public void resta(View view) {
     }
 }
