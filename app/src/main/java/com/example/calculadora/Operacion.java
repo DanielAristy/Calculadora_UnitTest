6ea17ae6a1137c286d7dff09edcf50f07a2fc038
DanielAristy <danielariaty@gmail.com>
2020-06-26 14:32:46 -0500

    Generando el parseo de los atributos de los metodos.

--- app/src/main/java/com/example/calculadora/Operacion.java
+++ app/src/main/java/com/example/calculadora/Operacion.java
@@ -3,17 +3,17 @@ package com.example.calculadora;
 public class Operacion {
 
     public String suma(String numero1, String numero2){
-        int resultado = Integer.valueOf(numero1) + Integer.valueOf(numero2);
+        int resultado = Integer.parseInt(numero1) + Integer.parseInt(numero2);
         return String.valueOf(resultado);
     }
 
     public String resta(String numero1, String numero2){
-        int resultado = Integer.valueOf(numero1) - Integer.valueOf(numero2);
+        int resultado = Integer.parseInt(numero1) - Integer.parseInt(numero2);
         return String.valueOf(resultado);
     }
 
     public String multiplicacion(String numero1, String numero2){
-        int resultado = Integer.valueOf(numero1) * Integer.valueOf(numero2);
+        int resultado = Integer.parseInt(numero1) * Integer.parseInt(numero2);
         return String.valueOf(resultado);
     }
 
